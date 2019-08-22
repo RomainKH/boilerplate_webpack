7a19abf8a2992bac016329e80197edadd8b6743c
RomainKH <romain.khanoyan@gmail.com>
2019-08-23 01:05:42 +0200

    :tada: init of boilerplate

---
+++ bundler/webpack.common.js
@@ -0,0 +1,80 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+module.exports = {
+    devtool: 'source-map',
+    devServer: 
+    {
+        contentBase: './dist',
+        open: true,
+        hot: true
+    },
+    entry: 
+    {
+        main: './src/index.js'
+    },
+    output:
+    {
+        filename: 'bundle.[hash].js',
+        path: path.resolve(__dirname, '../dist')
+    },
+    plugins:
+    [
+        new CopyWebpackPlugin([{from: 'static'}]),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, '../src/index.html')
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'exemple.html',
+            template: path.resolve(__dirname, '../src/pages/exemple.html')
+        })
+    ],
+    module:
+    {
+        rules:
+        [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use:
+                [
+                    'babel-loader'
+                ]
+            },
+            {
+                test: /\.(jpg|png|svg|gif)$/,
+                use:
+                [
+                    {
+                        loader: 'file-loader',
+                        options:
+                        {
+                            outputPath: 'images/'
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(woff2|woff|otf|eot|ttf)$/,
+                use:
+                [
+                    {
+                        loader: 'file-loader',
+                        options:
+                        {
+                            outputPath: 'fonts/'
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.html$/,
+                use:
+                [
+                    'html-loader'
+                ]
+            }
+        ]
+    }
+}
